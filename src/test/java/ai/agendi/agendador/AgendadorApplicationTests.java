5bc6aecf0ab8e85c7fdaf3a109a465072e27602e
Pedro Alves <133861580+pphvaz@users.noreply.github.com>
2025-01-05 14:46:23 -0300

    Criado projeto Agendador

---
+++ src/test/java/ai/agendi/agendador/AgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.agendi.agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
